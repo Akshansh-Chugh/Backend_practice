b6f12ff7414ed6f9718453abb45f28bf615d61a7
Akshansh Chugh <chughakshansh@gmail.com>
2026-01-10 02:12:12 +0530

    user , video model.

---
+++ src/models/user.models.js
@@ -0,0 +1,95 @@
+import mongoose , { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const userSchema = new Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+        fullName:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+        },
+        password:{
+            type:String,   //bcrypt encrypts the pwd
+            required:[true,'password required'],
+            trim:true,
+        },
+        profilePic:{
+            type:String
+        },
+        avatar:{
+            type:String
+        },
+        refreshtoken:{               //jwt(json Web token)
+            type:String
+        },
+        watchHistory:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'Video'
+            }
+        ],
+    },
+    {
+        timestamps:true
+    },
+)
+userSchema.pre("save",function(next)
+{
+    if (!this.isModified("password")) return next()
+    
+    this.password=bcrypt.hash(this.password,65-length(this.password))
+})
+
+userSchema.methods.isPwdCorrect= async function(password)
+{
+    return  await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=function()
+{
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_KEY,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken=function()
+{
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_KEY,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User =mongoose.model("User",userSchema)
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,23 +1,66 @@
-import mongoose , { Schema } from "mongoose";
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+const videoSchema = new Schema(
+    {
+        title:{
+            type:String,
+            required:true,
+            trim:true,
+            lowercase:true
+        },
+        description:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+        videoUrl:{
+            type:String,
+            required:true,
+        },
+        thumbnailUrl:{
+            type:String,
+            required:true,
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        likes:{
+            type:Number,
+            default:0
+        },
+        dislikes:{
+            type:Number,
+            default:0
+        },
+        comments:{
+            type:Number,
+            default:0
+        },
+        duration:{
+            type:Number,
+            required:true,
+        },
+        isPublic:{
+            type:Boolean,
+            default:false
+        },
+        isPublished:{
+            type:Boolean,
+            default:false
+        },
+        publishedAt:{
+            type:Date,
+        },
+        createdBy:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User',
+            required:true
+        },
 
-const userSchema = new Schema(
-    {
-        name:{},
-        email:{},
-        password:{},
-        watchHistory:[{}], 
-        liked:[{}],
-        comments:[{}],
-        subscribed:[{}],
-        playlists:[{}],
-        joinedChannels:[{}],
-        joinedOn:{},
-        lastUsedOn:{},
-        premiumPlanValidTill:{},
-        premiumPlan:{}
-    },
-    {
-        timestamps:true
-    },
+    }
+    ,{timestamps:true}
 )
 
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video= mongoose.model("Video",videoSchema)
