d2969a7795b108c7a783ec0c34b2553fc9e0a481
Akshansh Chugh <chughakshansh@gmail.com>
2026-01-17 22:38:47 +0530

    insert generateTokens

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,18 @@
 import {Router} from "express"
-import {registerUser , loginUser , logoutUser} from "../controllers/user.controller.js"
+import {registerUser , loginUser , logoutUser, generateTokens} from "../controllers/user.controller.js"
 import upload from "../middleware/multer.js"
 import Jwt from "../middleware/auth.middleware.js"
 
 const router=Router()
 
-router.route("/login").post(upload.upload.none(),loginUser)
-router.route("/register").post(
-    upload.upload.fields([{name:"avatar",maxCount:1} ,{name:"profilePic",maxCount:1}]),
+router.route("/login").post(loginUser)
+router.route("/register")
+.post(upload.upload.fields([{name:"avatar",maxCount:1} ,{name:"profilePic",maxCount:1}]),
     registerUser)
-// router.route("/login").post(loginUser)
-router.route("/logout").post(upload.upload.fields([{name:"avatar",maxCount:1} ,{name:"profilePic",maxCount:1}]),Jwt,logoutUser)
+
+// secure
+router.route("/logout").post(Jwt,logoutUser)
+router.route("/refreshTokens").post(generateTokens)
+
 
 export default router
